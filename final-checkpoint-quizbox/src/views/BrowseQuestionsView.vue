efd148f0b4d072a0ae55bf39aa303b205ca4c791
Lorenz Siebenwurst <lo@ohn-line.de>
2022-06-29 17:43:14 +0200

    feat: BrowseQuestionsView works

--- final-checkpoint-quizbox/src/views/BrowseQuestionsView.vue
+++ final-checkpoint-quizbox/src/views/BrowseQuestionsView.vue
@@ -1,14 +1,26 @@
 <template>
   <h2>BROWSE QUESTIONS</h2>
   <p>Choose a category with questions</p>
-  <select name="questionsAmount" id="questionsAmount">
-    <option v-for="(item, index) in questionsObject" :key="index" value="index">
+  <select
+    name="questionsAmount"
+    id="questionsAmount"
+    @change="categorySelected"
+  >
+    <option
+      v-for="(item, index) in questionsObject"
+      :key="index"
+      :value="index"
+    >
       {{ item.name }}
     </option>
   </select>
   <h3>Questions from the chapter</h3>
   <ul>
-    <li v-for="question in questionsObject[0].questions" :key="question">
+    <li
+      v-for="(question, index) in questionsObject[currentCategoryIndex]
+        .questions"
+      :key="index"
+    >
       {{ question }}
     </li>
   </ul>
@@ -17,9 +29,15 @@
 <script>
 export default {
   name: "BrowseQuestionsView",
+  methods: {
+    categorySelected(event) {
+      this.currentCategoryIndex = event.target.value;
+    },
+  },
 
   data() {
     return {
+      currentCategoryIndex: 0,
       questionsObject: [
         {
           name: "Basics Html CSS",
