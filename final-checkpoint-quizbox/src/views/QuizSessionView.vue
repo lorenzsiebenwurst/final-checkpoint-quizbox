01db2af14e4c64b017d1375aab60073c3049b6b9
Lorenz Siebenwurst <lo@ohn-line.de>
2022-06-30 07:15:17 +0200

    basics of QuizSessionView

--- final-checkpoint-quizbox/src/views/QuizSessionView.vue
+++ final-checkpoint-quizbox/src/views/QuizSessionView.vue
@@ -1,9 +1,35 @@
 <template>
-  {{ $route.query }}
+  <h2>QUIZBOX SESSION</h2>
+  <p>Question count</p>
+  <div>{{ questionNumber }}/{{ amountToFullNumber }}</div>
+  <p>
+    {{ questionsObject[sessionAttributes.id].questions[questionNumber] }}
+  </p>
+  <button @click="clickNext">Next</button>
 </template>
 
 <script>
+import questionsObject from "@/data/questions.js";
 export default {
   name: "QuizSessionView",
+  data() {
+    return {
+      questionsObject,
+      sessionAttributes: this.$route.query,
+      questionNumber: 0,
+    };
+  },
+  computed: {
+    amountToFullNumber() {
+      return this.sessionAttributes.amount + "0";
+    },
+  },
+  methods: {
+    clickNext() {
+      if (this.questionNumber < this.amountToFullNumber) {
+        this.questionNumber++;
+      }
+    },
+  },
 };
 </script>
